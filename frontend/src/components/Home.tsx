44b55a759ca5af23039b737b9992d8d3a1822262
Mukul Singhal <work.mukulsinghal@gmail.com>
2025-05-06 15:24:34 +0530

    home page nav completed

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Search, ShoppingBag } from 'lucide-react';
+import '../styles/Home.css';
 
 const Home: React.FC = () => {
   return (
-    <div className="p-4">
-      <Link 
-        to="/cart" 
-        className="text-blue-600 hover:text-blue-800 underline"
-      >
-        Go to Cart
-      </Link>
+    <div className="home-page">
+      <nav className="navbar">
+        <div className="navbar-left">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/shop" className="nav-link">Shop</Link>
+          <Link to="/about" className="nav-link">About</Link>
+          <Link to="/contact" className="nav-link">Contact us</Link>
+        </div>
+        <div className="navbar-center">
+          <img src="/logo.png" alt="Thanda House Logo" className="logo" />
+        </div>
+        <div className="navbar-right">
+          <Search className="icon" />
+          <ShoppingBag className="icon" />
+          <button className="get-thanda-button">Get Thanda</button>
+        </div>
+      </nav>
+      {/* Add your home page content here */}
     </div>
   );
 };
