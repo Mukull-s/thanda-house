a0b91bbbf0dac798b1fd7ccad6190a5f27095fb6
Mukul Singhal <work.mukulsinghal@gmail.com>
2025-05-15 22:41:12 +0530

    updated route of category

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -21,25 +21,3 @@ router.put('/:id', protect, isAdmin, updateCategory);
 router.delete('/:id', protect, isAdmin, deleteCategory);
 
 module.exports = router; 
-const router = express.Router();
-const { protect, isAdmin } = require('../middlewares/authMiddleware');
-const {
-  getCategories,
-  getCategoryById,
-  createCategory,
-  updateCategory,
-  deleteCategory,
-  getCategoryProducts
-} = require('../controllers/categoryController');
-
-// Public routes
-router.get('/', getCategories);
-router.get('/:id', getCategoryById);
-router.get('/:id/products', getCategoryProducts);
-
-// Admin routes
-router.post('/', protect, isAdmin, createCategory);
-router.put('/:id', protect, isAdmin, updateCategory);
-router.delete('/:id', protect, isAdmin, deleteCategory);
-
-module.exports = router; 
