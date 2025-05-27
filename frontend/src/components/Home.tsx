ac4a18de298332ac456d0d650291a5cdfbceca53
Mukul Singhal <work.mukulsinghal@gmail.com>
2025-05-27 23:45:27 +0530

    fixed the ui of homepage and added bottles

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import { Search, ShoppingBag } from 'lucide-react';
+import Navbar from './Navbar';
 import '../styles/Home.css';
 
 const Home: React.FC = () => {
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-left">
-          <a href="/" className="nav-link">Home</a>
-          <a href="/shop" className="nav-link">Shop</a>
-          <a href="/about" className="nav-link">About</a>
-          <a href="/contact" className="nav-link">Contact us</a>
-        </div>
-        <div className="navbar-center">
-          <img src="/logo.png" alt="Thanda House Logo" className="navbar-logo" />
-        </div>
-        <div className="navbar-right">
-          <Search className="nav-icon" />
-          <ShoppingBag className="nav-icon" />
-          <button className="get-thanda-btn">Get Thanda</button>
-          <a href="#" className="nav-link login-link">Signup/Login</a>
-        </div>
-      </nav>
+      <Navbar />
       <div className="home-background">
         <div className="home-logo-section">
           <img src="/logo.png" alt="Thanda House Premium Beer" className="home-main-logo" />
